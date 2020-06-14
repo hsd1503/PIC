84521aa45668f7a7fcf1b151c8a0f1d388e63f8a
hostrue <watson9281@gmail.com>
2020-06-13 22:00:44 -0400

    update

--- exp.py
+++ exp.py
@@ -14,6 +14,12 @@ from baseline_prism_iii import prism_iii
 pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
 
+"""
+AUROC, std, AUPRC, std
+[0.5949, 0.0000, 0.1534, 0.0000]
+[0.7100, 0.1730, 0.8277, 0.2930]
+[0.0157, 0.0076, 0.0142, 0.0095]
+"""
 
 def my_eval(gt, y_pred_proba):
     """
@@ -51,9 +57,11 @@ if __name__ == "__main__":
     y_pred = prism_iii(df)
     print('PRISM_III', my_eval(y, y_pred))
 
+    all_res = []
     kf = KFold(n_splits=5, shuffle=True, random_state=seed)
     feature_scores = []
     for train_index, test_index in kf.split(X):
+        tmp_res = []
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         print(X_train.shape, X_test.shape)
@@ -63,22 +71,30 @@ if __name__ == "__main__":
         m = LR()
         m.fit(X_train, y_train)
         y_pred = m.predict_proba(X_test)[:,1]
-        print('LR', my_eval(y_test, y_pred))
+        t_res = my_eval(y_test, y_pred)
+        print('LR', t_res)
+        tmp_res.extend(list(t_res.values()))
         
         # RF
         m = RF(n_estimators=100, random_state=seed)
         m.fit(X_train, y_train)
         y_pred = m.predict_proba(X_test)[:,1]
-        print('RF', my_eval(y_test, y_pred))
+        t_res = my_eval(y_test, y_pred)
+        print('RF', t_res)
+        tmp_res.extend(list(t_res.values()))
         
         feature_scores.append(m.feature_importances_)
+        all_res.append(tmp_res)
         
     feature_scores = np.mean(np.array(feature_scores), axis=0)
     df_imp = pd.DataFrame({'col':x_cols, 'score':feature_scores})
     df_imp = df_imp.sort_values(by='score', ascending=False)
     plt.plot(df_imp.score.values[1:])
     
-    
+    all_res = np.array(all_res)
+    res_mean = np.mean(all_res, axis=0)
+    res_std = np.std(all_res, axis=0)
+    print(res_mean, res_std)
     
     
     
